cebf830541cda3d29dff0e269a0dc2ad0e1dd8f6
Der Robert <erictainment@live.de>
2025-02-16 03:23:56 +0100

    prepare make.cmd

--- src/make.cmd
+++ src/make.cmd
@@ -1,3 +1,18 @@
 @echo off
-set src=%~dp0
-set bin=%src%..\
+	set src=%~dp0
+	set bin=%src%..
+
+:prepare_folders
+	for %%i in (
+		blockstates
+		items
+		lang
+		models
+		optifine
+		textures
+	) do (
+		del /q /s %bin%\%%i\* 2>nul
+		rmdir /q /s %bin%\%%i 2>nul
+		mkdir /q /s %bin%\%%i 2>nul
+	)
+
